cf84e121d1efe3339a9f8d5159d210cbdc2c926b
anisegwl <anisgnawali34@gmail.com>
2024-09-24 04:57:33 -0700

    I made new program of todolist

--- Todolist.py
+++ Todolist.py
@@ -1,11 +1,14 @@
-class Todolist:
-    def create_task():
-        pass
-    def view_task():
-        pass
-    def completed_task():
-        pass
-    def delete_task():
-        pass
+def main():
+    task = []
+while True:
+    print("====To Do List====")
+    print("1. Add Task")
+    print("2. Show Tasks")
+    print("3. Mark the task as done")
+    print("4 . Exit the app")
+    choice = input("Enter your choice : ")
+if choice ==  "1":
+    print()
+task_num = int(input("Enter how many task would you like to add  : "))
 
     
